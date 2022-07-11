373874afa71fbb7d26aecf3c177781dd03ea9b4e
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2022-07-11 16:37:48 +0530

    Add files via upload

---
+++ day6/program1.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MyException : public exception
+{
+    const char *message;
+    string param;
+
+    public:
+        MyException(const char *message, string param) 
+            : message(message), param(param) {
+
+        }
+
+        const char* what() const throw()
+        {
+            cout << "\nMyException [ " << param << " ]\n";
+            return message;
+        }
+};
+
+int main()
+{
+    try
+    {
+        int a, b;
+        cout << "Enter two elements - ";
+        cin >> a >> b;
+        if (b == 0) {
+            throw MyException("\nInvalid division", "Division by zero");
+        }
+        cout << "\nDivision result - " << (a / b) << "\n\n";
+
+        char* p = new (std::nothrow) char [9999999999999999];
+
+        if (!p) {
+            throw MyException("\nArray index is invalid", "Index out of range");
+        }
+        else {
+            std::cout << "Succeeded!\n";
+            delete[] p;
+        }
+
+        string str;
+        cout << "Enter a number - ";
+        cin >> str;
+        int num = stoi(str);
+        cout << "Number - " << num << "\n";
+
+        string s2;
+        cout << "Enter a string - ";
+        cin >> s2;
+        int sLen = s2.length(), sIndex;
+        cout << "Enter an index to access the string - ";
+        cin >> sIndex;
+        if (sIndex < 0 or sIndex >= sLen){
+            throw MyException("\nString index is invalid", "Index out of range");
+        }
+        cout << "String value - " << s2[sIndex] << "\n";
+
+        int arrLen;
+        cout << "Enter the size for array - ";
+        cin >> arrLen;
+        int arr[arrLen];
+        cout << "Enter an array of integers - ";
+        for (int i = 0; i < arrLen; i++){
+            cin >> arr[i];
+        }
+        int arrIndex;
+        cout << "Enter an index to access the array - ";
+        cin >> arrIndex;
+        if (arrIndex < 0 or arrIndex >= arrLen){
+            throw MyException("\nArray index is invalid", "Index out of range");
+        }
+        cout << "Array value - " << arr[arrIndex] << "\n";
+
+        int n;
+        cout << "Enter a number greater than 0 - ";
+        cin >> n;
+        if (n < 0){
+            throw "You have entered a negative number !";
+        }
+        std::cout << "The number entered is " << n;
+    }
+    catch (const exception &e)
+    {
+        cout << "\n" << e.what() << "\n";
+    }
+    catch (...)
+    {
+        cout << "\nException occurred\n";
+    }
+
+    cout << "\nException Handling Accomplished\n\n";
+
+    return 0;
+}
