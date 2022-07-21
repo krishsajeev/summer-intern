bb77f958479eadd5bd3d2f9b811119a407cd5c88
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2022-07-21 18:00:02 +0530

    Update Helper.h

--- PROTOCOL-BUFFERS/Helper.h
+++ PROTOCOL-BUFFERS/Helper.h
@@ -105,3 +105,91 @@ public:
 		return true;
 	}
 };
+
+int max(int a, int b) {
+	return (a > b) ? a : b;
+}
+
+void LCS(std::string X, std::string Y, int m, int n) {
+	int** L = new int* [m];
+	for (int i = 0; i < m; i++)
+	{
+		L[i] = new int[n];
+	}
+
+	/* Following steps build L[m+1][n+1] in bottom up
+	  fashion. Note that L[i][j] contains length of LCS of
+	  X[0..i-1] and Y[0..j-1] */
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (i == 0 || j == 0)
+				L[i][j] = 0;
+			else if (X[i - 1] == Y[j - 1])
+				L[i][j] = L[i - 1][j - 1] + 1;
+			else
+				L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+		}
+	}
+
+	// Following code is used to print LCS
+	int index = L[m - 1][n - 1] + 1;
+
+	// Create a character array to store the lcs string
+	char* lcs = new char[index + 1];
+
+	lcs[index] = '\0'; // Set the terminating character
+
+	// Start from the right-most-bottom-most corner and
+	// one by one store characters in lcs[]
+	int i = m, j = n;
+	while (i > 0 && j > 0) {
+		// If current character in X[] and Y are same, then
+		// current character is part of LCS
+		if (X[i - 1] == Y[j - 1]) {
+			lcs[index - 1]
+				= X[i - 1]; // Put current character in result
+			i--;
+			j--;
+			index--; // reduce values of i, j and index
+		}
+
+		// If not same, then find the larger of two and
+		// go in the direction of larger value
+		else if (L[i - 1][j] > L[i][j - 1])
+			i--;
+		else
+			j--;
+	}
+
+	/*
+	for (int i = 0; i < m; i++)
+	{
+		delete[] L[i];
+	}
+	delete[] L;
+	*/
+
+	// Print the lcs
+	std::cout << lcs;
+
+	//delete[] lcs;
+}
+
+std::string stringify(DATA::File file) {
+	std::string temp = file.lines(0);
+	for (int i = 1; i < file.lines_size(); i++) {
+		temp = temp + file.lines(i);
+	}
+	return temp;
+}
+
+void loadDiff(DATA::Version v1, DATA::Version v2) {
+	for (auto it = v1.files().begin(); it != v1.files().end(); it++) {
+		if (v2.files().contains(it->first)) {
+			std::string str1 = stringify(it->second);
+			std::string str2 = stringify((v2.files().find(it->first))->second);
+			LCS(str1, str2,str1.length(),str2.length());
+		}
+	}
+}
+
